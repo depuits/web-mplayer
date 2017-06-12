8e0f833a747d769c0652cc39f9fba10cda369985
depuits <colmanjoeri@msn.com>
2017-06-12 11:56:56 +0200

    implemented one vote per client counting clients on connection implemented rest api generic command endpoint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var config = require('config');
 var express = require('express');
+var bodyParser = require("body-parser");
 
 var ctrl = require('./lib/controller');
 
@@ -10,16 +11,21 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var port = config.get('port');
+var clients = new Map();
 
 ctrl.on('statuschange', (stat) => {
 	sendPlayerStatus();
 });
-
+ctrl.on('votechange', (stat) => {
+	sendVoteStatus();
+});
 ctrl.playlist.on('updated', () => {
 	sendPlaylistUpdate();
 });
 
 function handleCommand(data) {
+	console.log('fp: ' + data.fp);
+	
 	switch(data.cmd) {
 		case 'togglePlay':
 			ctrl.player.togglePause();
@@ -31,10 +37,10 @@ function handleCommand(data) {
 			ctrl.player.pause();
 			break;
 		case 'next':
-			ctrl.next();
+			ctrl.next(data.fp);
 			break;
 		case 'veto':
-			ctrl.veto();
+			ctrl.veto(data.fp);
 			break;
 		case 'volume':
 			ctrl.player.volume(data.value);
@@ -45,36 +51,71 @@ function handleCommand(data) {
 	}
 }
 
+function sendPlayerStatus(socket) {
+	socket = socket || io;
+	socket.emit('status', ctrl.status);
+}
+function sendVoteStatus(socket) {
+	var data = ctrl.votes;
+	data.all = clients.size;
+	socket = socket || io;
+	socket.emit('votes', data);
+}
 function sendPlaylistUpdate(socket) {
 	socket = socket || io;
 	//TODO we should filter the playlist to not send the complete file path
 	socket.emit('playlist', ctrl.playlist.get());
 }
 
-function sendPlayerStatus(socket) {
-	socket = socket || io;
-	socket.emit('status', ctrl.status);
-}
-
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get('/playlist', function(req, res, next){
 	res.status(200).json(ctrl.playlist.get());
 });
-
+app.get('/cmd', function(req, res, next){
+	req.body.fp = req.ip;
+	handleCommand (req.body);
+	res.redirect('/');
+});
 app.get('/:cmd', function(req, res, next){
-	handleCommand ({ cmd: req.params.cmd }); //TODO replace witch cmd endpoint and parse body
+	handleCommand ({ cmd: req.params.cmd, fp: req.ip });
 	res.redirect('/');
 });
 
 io.on('connection', function (socket) {
-	var fp = socket.handshake.query.fp;
-	console.log('client connected: ' + fp);
+	var fp = socket.handshake.query.fp; //socket.request.connection.remoteAddress;
+	// if this client does not already have another socket open then its a new one
+	// clients using the rest api are not counted towards active clients
+	var clSockets = clients.get(fp);
+	if(clSockets === undefined) {
+		clSockets = [];
+		clients.set(fp, clSockets);
+		console.log('client connected: ' + fp);
+		sendVoteStatus();
+	}
+	
+	clSockets.push(socket);
+	
 	sendPlaylistUpdate(socket);
-	sendPlayerStatus();
+	sendPlayerStatus(socket);
+	sendVoteStatus(socket);
 	socket.on('command', (data) => {
 		data.fp = fp;
 		handleCommand (data);
+	});
+	socket.on('disconnect', () => {
+		var index = clSockets.indexOf(socket);
+		if (index > -1) {
+			clSockets.splice(index, 1);
+		}
+		
+		// if this is the last socket of this client then the client disconnected
+		if(clSockets.length === 0) {
+			clients.delete(fp);
+			console.log('client disconnected: ' + fp);
+			sendVoteStatus();
+		}
 	});
 });
 
