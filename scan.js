5a740ed0e03fd047466bf3af871ef07240efcb44
depuits <colmanjoeri@msn.com>
2017-06-21 15:59:43 +0200

    promise impl in audiolib and scan

--- scan.js
+++ scan.js
@@ -4,11 +4,12 @@ var mongo = require('./lib/mongo');
 var audiolib = require('./lib/audiolib');
 
 // initialize db connection
-mongo.connect((err) => {
-	if (err) {
-		console.log(err);
-		process.exit();
-	}
-	// update library
-	audiolib.update();
+mongo.connect().then(() => {
+	return audiolib.update();
+}).then((data) => {
+	console.log('Scanned ' + data.length + ' items.');
+	process.exit();
+}).catch((err) => {
+	console.log(err);
+	process.exit();
 });
